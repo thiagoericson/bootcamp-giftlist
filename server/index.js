b1bc1a72c16050b0463136608afad60a451fd6d0
Ericson Cabral <tncl@gft.com>
2023-10-23 16:41:47 -0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require("../utils/MerkleTree");
 
 const port = 1225;
 
@@ -8,14 +10,19 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList);
+const root = merkleTree.getRoot();
+
+const MERKLE_ROOT = root;
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
+  const { name , proof } = body;
+
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof,name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
